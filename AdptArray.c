be3f1135032781da4ad7030c24396a96017bc3f3
Hadar Bitan <86705118+HadarBitan@users.noreply.github.com>
2023-03-22 09:22:21 +0200

    Add files via upload
    final upload with comments

--- AdptArray.c
+++ AdptArray.c
@@ -17,6 +17,7 @@ struct AdptArray_
 
 PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC del,PRINT_FUNC print)
 {
+    //aloocate a memory for the struct
     PAdptArray adpt = (PAdptArray)malloc(sizeof(struct AdptArray_));
     if (adpt == NULL) {
         printf("Memory not allocated.\n");
@@ -32,15 +33,19 @@ PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC del,PRINT_FUNC print)
 
 void DeleteAdptArray(PAdptArray padt)
 {
+    //check if the struct is null, if it is null then there isn't something to delete and we finish
     if(padt == NULL)
     {
         return;
     }
+    //if the struct exists but the the array is null then all we left to do is free the struct
     if(padt->pelmt == NULL)
     {
         free(padt);
         return;
     }
+    //if everything exists then we have to free all the elements according to the delete function of the ADT array
+    //after we delete all the elements properly we free the array of element and the struct itself
     if (padt->pelmt != NULL)
     {
         for(int i = 0; i < padt->size; i++)
@@ -55,9 +60,9 @@ void DeleteAdptArray(PAdptArray padt)
     }
 }
 
-
 Result SetAdptArrayAt(PAdptArray padt, int index, PElement element)
 {
+    //in this function we get an index, an element and an ADT array struct
     PElement * temp = (PElement *) malloc((index + 1) * sizeof(PElement));
     if (padt == NULL)
         return FAIL;
@@ -79,6 +84,7 @@ Result SetAdptArrayAt(PAdptArray padt, int index, PElement element)
             temp[i] = NULL;
         }
     // Free old array and save new array
+    //here we have to properly free and delete the old array so we need to use ahe array's delete function if we get a non null elemnt
         for(int i = 0; i < padt->size; i++)
         {
             if(padt->pelmt[i] != NULL)
@@ -89,7 +95,7 @@ Result SetAdptArrayAt(PAdptArray padt, int index, PElement element)
         free(padt->pelmt);
         padt->pelmt = temp;
     }
-    // Delete Previous Elem and Set New Elem
+    // Delete Previous Element if exists and Set New Elem
     if(padt->pelmt[index] != NULL)
     {
         padt->del(padt->pelmt[index]);
@@ -102,6 +108,8 @@ Result SetAdptArrayAt(PAdptArray padt, int index, PElement element)
 
 PElement GetAdptArrayAt(PAdptArray padt, int index)
 {
+    //in this function we get an index and a struct and return the element in the index
+    //error checks, if the struct is null or its' array is null to the index is not in our bounds or the element itself is null then return null
     if(padt == NULL || index > padt->size || padt->pelmt == NULL || padt->pelmt[index] == NULL)
     {
         return NULL;
@@ -111,6 +119,7 @@ PElement GetAdptArrayAt(PAdptArray padt, int index)
 
 int GetAdptArraySize(PAdptArray padt)
 {
+    //if the ADT array in null we return -1 else we get the size from the struct
     if(padt == NULL)
     {
         return -1;
@@ -120,8 +129,10 @@ int GetAdptArraySize(PAdptArray padt)
 
 void PrintDB(PAdptArray padt)
 {
+    //In this function we print all the elements in the array according to the print function belong to the array
     for(int i = 0; i< padt->size; i++)
     {
+        //checking if the element is null, if it is null we dont print it
         if(padt->pelmt[i] != NULL)
             padt->print(padt->pelmt[i]);
     }
